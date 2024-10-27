174c329aa0ed53c24e83fbbdb3fabeb53ba78e6c
Prajwal K <prajwalkreddy17@gmail.com>
2024-10-27 13:20:51 +0530

    fix categories listing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { categories } from "@/constants/categories";
 
 export default function Home() {
   return (
-    <section className="flex mx-4 md:px-5 gap-3 pt-4 pb-2 justify-between text-sm font-medium border-b-2 overflow-scroll">
+    <section className="flex mx-4 md:px-5 gap-3 pt-4 pb-2 justify-between text-sm font-medium border-b-2 overflow-x-auto overflow-y-hidden whitespace-nowrap">
       {categories.map((category, index) => (
         <CategoryBox
           key={index}
