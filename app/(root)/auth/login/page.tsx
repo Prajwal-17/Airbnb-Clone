9e98cb32b8f958bc94c1bec70866ceb4bb272535
Prajwal K <prajwalkreddy17@gmail.com>
2024-11-13 18:40:13 +0530

    updated ui of login form

--- app/(root)/auth/login/page.tsx
+++ app/(root)/auth/login/page.tsx
@@ -2,12 +2,11 @@ import SignInForm from "@/components/SignInForm";
 
 export default function SignIn() {
   return (<>
-    <section>
-      <div>
+    <section className="min-h-screen flex justify-center items-center ">
+      <div className=" w-full px-5 sm:w-8/12 md:w-8/12 lg:w-5/12 md:px-9 py-7 rounded-lg shadow-xl">
 
-        <h3>Login</h3>
-        <h1>Welcome back</h1>
-        <div>Login to your Account!</div>
+        <h1 className="text-3xl font-bold my-2">Welcome back</h1>
+        <div className="text-gray-500">Login to your Account!</div>
 
         <SignInForm />
 
