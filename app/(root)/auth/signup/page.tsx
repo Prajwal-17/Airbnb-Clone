87b427a2a9f5725d59bab02fb7566078029346b6
Prajwal K <prajwalkreddy17@gmail.com>
2024-11-13 23:11:14 +0530

    updated signup form ui

--- app/(root)/auth/signup/page.tsx
+++ app/(root)/auth/signup/page.tsx
@@ -2,12 +2,11 @@ import SignUpForm from "@/components/SignUpForm";
 
 export default function SignUp() {
   return (<>
-    <section>
-      <div>
+    <section className="min-h-screen flex justify-center items-center">
+      <div className=" w-full px-5 sm:w-8/12 md:w-8/12 lg:w-5/12 md:px-9 py-7 rounded-lg shadow-xl">
 
-        <h3>Sign Up</h3>
-        <h1>Welcome to Airbnb</h1>
-        <div>Create an Account</div>
+        <h1 className="text-3xl font-bold my-2">Welcome to Airbnb</h1>
+        <div className="text-gray-500">Create an Account</div>
 
         <SignUpForm />
 
